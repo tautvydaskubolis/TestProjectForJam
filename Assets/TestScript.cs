884c147cc2567f8ade85fe7816c4b47861ba17ee
tautvydasKubolis <tautvydas.kubolis@unity3d.com>
2020-01-30 22:38:08 +0200

    test

---
+++ Assets/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using TilemapGenerator;
+
+public class TestScript : MonoBehaviour
+{
+    public Tilemap tilemap;
+    void Start()
+    {
+        MapGenerator tg = new MapGenerator();
+        tg.DrawTiles(tilemap, "Tilemap");
+    }
+}
